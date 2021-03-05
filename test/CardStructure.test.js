a7cdfd02cf299f3ab5dac6b88a74cde18c97386b
The Creator <creator@8bitians.com>
2021-03-05 16:58:05 -0500

    Update tests

--- test/CardStructure.test.js
+++ test/CardStructure.test.js
@@ -1,39 +1,36 @@
 const { accounts, contract } = require('@openzeppelin/test-environment');
-const [ owner ] = accounts;
-
-const { expect } = require('chai');
 
 const CardStructure = contract.fromArtifact('CardStructure');
+let myContract;
 
 describe('CardStructure', function () {
+  const [ owner, user ] = accounts;
 
   beforeEach(async function () {
-    this.contract = await CardStructure.new({ from: owner });
+    myContract = await CardStructure.new({ from: owner });
   });
 
   it('deployer is owner', async function () {
-    expect(await this.contract.owner()).to.equal(owner);
+    expect(await myContract.owner()).toEqual(owner);
   });
 
   it('create series and card type from owner', async function () {
-    await this.contract.createCardSeries('New Series', { from: owner });
-    const newSeries = await this.contract.retrieveSeries();
-    expect((newSeries[0].name)).to.equal('New Series');
-    await this.contract.createCardType('New Type', 0, 3, { from: owner });
-    const newType = await this.contract.retrieveTypes();
-    expect((newType[0].name)).to.equal('New Type');
+    await myContract.createCardSeries('New Series', { from: owner });
+    const newSeries = await myContract.retrieveSeries();
+    expect(newSeries[0].name).toEqual('New Series');
+    await myContract.createCardType('New Type', 0, 3, { from: owner });
+    const newType = await myContract.retrieveTypes();
+    expect(newType[0].name).toEqual('New Type');
   })
 
   it('change name of series and type from owner', async function () {
-    await this.contract.createCardSeries('New Series', { from: owner });
-    const newSeries = await this.contract.retrieveSeries();
-    await this.contract.createCardType('New Type', 0, 3, { from: owner });
-    const newType = await this.contract.retrieveTypes();
-    await this.contract.updateSeriesName(0, 'New New Series', { from: owner });
-    const changedSeries = await this.contract.retrieveSeries();
-    expect((changedSeries[0].name)).to.equal('New New Series');
-    await this.contract.updateTypeName(0, 'New New Type', { from: owner });
-    const changedType = await this.contract.retrieveTypes();
-    expect((changedType[0].name)).to.equal('New New Type');
+    await myContract.createCardSeries('New Series', { from: owner });
+    await myContract.createCardType('New Type', 0, 3, { from: owner });
+    await myContract.updateSeriesName(0, 'New New Series', { from: owner });
+    const changedSeries = await myContract.retrieveSeries();
+    expect(changedSeries[0].name).toEqual('New New Series');
+    await myContract.updateTypeName(0, 'New New Type', { from: owner });
+    const changedType = await myContract.retrieveTypes();
+    expect(changedType[0].name).toEqual('New New Type');
   })
 });
